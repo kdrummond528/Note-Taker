df18fa996f3ff87971dbe8dd87df59e2c589a139
Karina Drummond <k.drummond528@gmail.com>
2022-11-04 07:35:51 -0700

    added to apiroutes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,3 +1,21 @@
 const notes = require('express').Router()
 const uuid = require('uuid');
 const fs = require("fs");
+
+// get
+notes.get('/', (req, res) => {
+
+})
+
+// post
+notes.post('/', (req, res) => {
+
+})
+
+// delete
+notes.delete('/', (req, res) => {
+
+})
+
+// export 
+module.exports = notes
