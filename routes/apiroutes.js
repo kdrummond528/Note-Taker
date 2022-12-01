7e6074121faf59c98ae5eca696b8b70039494995
Karina Drummond <k.drummond528@gmail.com>
2022-12-01 12:41:46 -0800

    modified apiroutes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -3,19 +3,25 @@ const uuid = require('uuid');
 const fs = require("fs");
 
 // get
-notes.get('/notes', (req, res) => {
-
+router.get('/notes', (req, res) => {
+    saveData
+        .retrieveNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err));
 })
 
 // post
-notes.post('/', (req, res) => {
-
+router.post('/', (req, res) => {
+    saveData
+        .addNote(req.body)
+        .then((note) => res.json(note))
+        .catch(err => res.status(500).json(err));
 })
 
 // delete
-notes.delete('/', (req, res) => {
+router.delete('/', (req, res) => {
 
 })
 
 // export 
-module.exports = notes
+module.exports = router;
