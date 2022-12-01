4d0c1b4cc3a85edd0f591a4430f87a9826c76873
Karina Drummond <k.drummond528@gmail.com>
2022-12-01 12:54:05 -0800

    modified apiroutes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -3,17 +3,26 @@ const app = require('express').Router();
 
 // get notes
 app.get('/notes', (req, res) => {
-
+    saveData
+        .retrieveNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err));
 });
 
 // post notes
 app.post('/', (req, res) => {
-
+    saveData
+        .addNote(req.body)
+        .then((note) => res.json(note))
+        .catch(err => res.status(500).json(err));
 });
 
 // delete notes
 app.delete('/', (req, res) => {
-
+    saveData
+        .deleteNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch(err => res.status(500).json(err));
 });
 
 // export 
