d7684d778a6dfe90f2b8dc8d9dcbb128611db6f1
Karina Drummond <k.drummond528@gmail.com>
2022-12-01 12:53:00 -0800

    modified apiroutes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,25 +1,14 @@
 const app = require('express').Router();
-const uuid = require('uuid');
-const fs = require("fs");
+
 
 // get notes
 app.get('/notes', (req, res) => {
-    fs.readFile('./db/db.json', (err, results) => {
-        if (err) {
-            throw err;
-        }
-        res.send(results)
-    })
+
 });
 
 // post notes
 app.post('/', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
-    const addNotes = req.body;
-    addNotes.id = uuid.v4();
-    notes.push(addNotes);
-    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
-    res.json(notes);
+
 });
 
 // delete notes
